387c66ad876916a7a38ae2bdfed89ecb88b21318
DhanushSS3 <support@livefxhub.com>
2025-06-10 22:37:57 -0700

    Added news endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,26 +3,18 @@
 from fastapi import APIRouter
 
 # Import individual routers from endpoints
-from app.api.v1.endpoints import users, groups # Import the users and groups routers
+from app.api.v1.endpoints import users, groups, orders, wallets, news, money_requests
 # Import the market data WebSocket router module
 from app.api.v1.endpoints import market_data_ws # Import the module
 
-from app.api.v1.endpoints import orders
-
-from app.api.v1.endpoints import money_requests
 # Create the main API router for version 1
 api_router = APIRouter()
 
-
+# Include all routers with appropriate prefixes and tags
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(groups.router, prefix="/groups", tags=["groups"])
 api_router.include_router(orders.router, tags=["orders"])
-
-
-api_router.include_router(market_data_ws.router, tags=["market_data"])
-
 api_router.include_router(money_requests.router, prefix="", tags=["Money Requests"])
-
-from app.api.v1.endpoints import wallets
-
-api_router.include_router(wallets.router, tags=["Money Requests"])
+api_router.include_router(wallets.router, tags=["Wallets"])
+api_router.include_router(news.router, tags=["News"])
+api_router.include_router(market_data_ws.router, tags=["market_data"])
